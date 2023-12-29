1a8d0cd1a83835710fcd57e805be1d77b93927b1
Ömer KÜLAK <omer.kulak@kitayazilim.com>
2023-12-29 15:06:39 +0300

    paketleme lilstesi geliştirmesi eklendi

---
+++ package_on_packinglist/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2023 Kıta Yazılım
+# License LGPLv3 or later (https://www.gnu.org/licenses/lgpl-3.0).
+
+{
+    'name': 'Package On Packinglist',
+    'summary': """Paketleme listesi satırlarında paket bilgisi""",
+    'description': """Paketleme listesi satırlarında paket bilgisi""",
+    'version': '16.0.1',
+    'license': 'LGPL-3',
+    'author': 'Kıta Yazılım',
+    'website': 'kitayazilim.com.tr',
+    'depends': ['stock_foreign_trade'],
+    'data': ['reports/report_stock_picking_document.xml'],
+    'demo': [
+    ],
+}
